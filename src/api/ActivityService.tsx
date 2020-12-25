d97b280eaecbd7e5f56d9581c1b04a566d42b93c
ths83 <ths.perron@gmail.com>
2020-12-25 17:48:06 +0100

    Closes #37 Change app to react-native library #28 #29 Update UI with new library

---
+++ src/api/ActivityService.tsx
@@ -0,0 +1,36 @@
+import {API} from 'aws-amplify';
+import {API_NAME} from '../config/AmplifyConfiguration';
+
+export async function createActivity(name: string, createdBy: string) {
+  const apiName = API_NAME;
+  const path = '/activity';
+  const myInit = {
+    headers: {},
+    body: {
+      name: name,
+      createdBy: createdBy,
+    },
+  };
+  console.log('Creating new activity...');
+  return await API.post(apiName, path, myInit);
+}
+
+export async function getActivityByUsername(username: string) {
+  const apiName = API_NAME;
+  const path = '/activities?username=' + username;
+  const myInit = {
+    headers: {},
+  };
+  console.log('Retrieving activity by username...');
+  return await API.get(apiName, path, myInit);
+}
+
+export async function getActivityById(id: string) {
+  const apiName = API_NAME;
+  const path = '/activities/' + id;
+  const myInit = {
+    headers: {},
+  };
+  console.log('Retrieving activity by id...');
+  return await API.get(apiName, path, myInit);
+}
---
+++ src/model/Activity.tsx
@@ -0,0 +1,24 @@
+export default class Activity {
+  id: string;
+  name: string;
+  createdBy: string;
+  expenses: string[];
+  activityStatus: string;
+  userStatus: [];
+
+  constructor(
+    id: string,
+    name: string,
+    createdBy: string,
+    expenses: string[],
+    activityStatus: string,
+    userStatus: [],
+  ) {
+    this.id = id;
+    this.name = name;
+    this.createdBy = createdBy;
+    this.expenses = expenses;
+    this.activityStatus = activityStatus;
+    this.userStatus = userStatus;
+  }
+}
