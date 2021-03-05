6852cf4472f192aaf302e8e5bc8123c48ddf65ae
ths83 <ths.perron@gmail.com>
2021-03-04 21:35:45 -0500

    Close #27 Add theme to addActivityPage

--- src/pages/addActivity/AddActivityPage.tsx
+++ src/pages/addActivity/AddActivityPage.tsx
@@ -1,16 +1,18 @@
 import {useNavigation} from '@react-navigation/native';
 import {useAtom} from 'jotai';
 import React, {useState} from 'react';
-import {View} from 'react-native';
+import {StyleSheet, View} from 'react-native';
 import {Button, Input, Text} from 'react-native-elements';
 import {ACTIVITY_API} from '../../api/ActivityApi';
 import {Pages} from '../../enums/Pages';
 import {Status} from '../../enums/Status';
 import activityAtom from '../../state/Activity';
+import {blue, grey} from '../../themes/colors';
+import {slarge} from '../../themes/size';
 
 const AddActivityPage = () => {
-  const [name, setName] = useState<string>('');
-  const [errorName, setErrorName] = useState<string>('');
+  const [name, setName] = useState('');
+  const [errorName, setErrorName] = useState('');
   const [status, setStatus] = useState<Status>(Status.IDLE);
 
   const [, setActivity] = useAtom(activityAtom);
@@ -43,9 +45,9 @@ const AddActivityPage = () => {
 
   return (
     <>
-      <View>
+      <View style={styles.activityInput}>
         <Input
-          placeholder="Name"
+          placeholder="Activity name"
           onChangeText={(text) => setName(text)}
           errorMessage={errorName}
         />
@@ -54,10 +56,33 @@ const AddActivityPage = () => {
         {status === Status.ERROR &&
           'An error occurred while adding new activity'}
       </Text>
-      <Button title={'Create'} onPress={createActivity} />
-      <Button title={'Back'} onPress={goBack} />
+      <View>
+        <Button
+          title={'Create'}
+          onPress={createActivity}
+          buttonStyle={styles.create}
+        />
+        <Button title={'Back'} onPress={goBack} buttonStyle={styles.back} />
+      </View>
     </>
   );
 };
+
+const styles = StyleSheet.create({
+  activityInput: {
+    flex: 1,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: 15,
+  },
+  create: {
+    backgroundColor: blue,
+  },
+  back: {
+    backgroundColor: grey,
+    paddingBottom: slarge,
+  },
+});
 
 export default AddActivityPage;
